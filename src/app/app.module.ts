380757333475fed184e0063ccd96cebdaef54929
hscst5161003 <5161003@st.hsc.ac.jp>
2018-06-09 23:47:38 +0900

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MatButtonModule, MatChipsModule, MatCardModule, MatListModule} from '@angular/material';
+import { MatButtonModule, MatChipsModule, MatCardModule, MatListModule, } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { EditorComponent } from './editor/editor.component';
+import { OutputComponent } from './output/output.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemListComponent,
+    EditorComponent,
+    OutputComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatCardModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
